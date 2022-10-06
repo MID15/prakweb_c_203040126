88c05c8e113b360938afdeeae13fb8db2a456960
MID15 <mindraw595@gmail.com>
2022-10-06 10:53:18 +0700

    menyelesaikan vid abstract (1)

---
+++ oop/#13 Abstract Class/abstract-class.php
@@ -0,0 +1,19 @@
+<?php
+//  Abstract Class
+//  Sebuah kelas yang tidak dapat di-instansiasi
+//  Kelas 'abstrak'
+//  Mendefinisikan interface untuk kelas lain yang menjadi turunannya
+//  Berperan sebagai 'kerangka dasar' untuk kelas turunannya
+//  Memiliki minimal 1 method abstrak 
+//  Digunakan dalam pewarisan' / inheritance untuk 'memaksakan' implementasi method abstrak yang sama untuk semua kelas turunannya
+
+// Abstract Class (2)
+// Semua kelas turunan, harus mengimplementasikan method abstrak yang ada di kelas abstraknya
+// Kelas abstrak boleh memiliki property / method reguler • Kelas abstrak boleh memiliki property / static method
+
+// Kenapa menggunakan kelas abstrak?
+// Merepresentasikan ide atau konsep dasar
+// Composition over Inheritance"
+// Salah satu cara menerapkan Polimorphism
+// Sentralisasi logic
+// Mempermudah pengerjaan tim
