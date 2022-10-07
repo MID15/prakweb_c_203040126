d603038af7f03cb3a67167f99be81676a8a7d12c
MID15 <mindraw595@gmail.com>
2022-10-07 20:47:09 +0700

    menyelesaikan vid View

--- mvc/app/controllers/About.php
+++ mvc/app/controllers/About.php
@@ -1,12 +1,21 @@
 <?php
-class About
+class About extends Controller
 {
-  public function index($nama = 'Indra', $pekerjaan = 'mahasiswa')
+  public function index($nama = 'Indra', $pekerjaan = 'mahasiswa', $umur = 20)
   {
-    echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+    $data['nama'] = $nama;
+    $data['pekerjaan'] = $pekerjaan;
+    $data['umur'] = $umur;
+    $data['judul'] = 'About';
+    $this->view('templates/header', $data);
+    $this->view('about/index', $data);
+    $this->view('templates/footer');
   }
   public function page()
   {
-    echo 'About/page';
+    $data['judul'] = 'Page';
+    $this->view('templates/header', $data);
+    $this->view('about/page');
+    $this->view('templates/footer');
   }
 }
